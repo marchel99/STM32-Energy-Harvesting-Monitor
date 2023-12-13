731045f7b9a1850852e671dbbbfef4a8701551c5
marchel99 <kubamarchelski@live.com>
2023-12-14 00:30:17 +0100

    new file:   .clang-format

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,26 +18,27 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+
+#include "gpio.h"
 #include "i2c.h"
 #include "rtc.h"
 #include "spi.h"
 #include "usart.h"
-#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 // #include "lcd.h"
 // #include "logo.c"
 
-#include <wchar.h>
-#include "hagl.h"
 #include "font6x9.h"
-#include "rgb565.h"
+#include "hagl.h"
 #include "max.h"
+#include "rgb565.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
+#include <wchar.h>
 
 /* USER CODE END Includes */
 
@@ -80,7 +81,7 @@ int __io_putchar(int ch)
     HAL_UART_Transmit(&huart2, &ch2, 1, HAL_MAX_DELAY);
   }
 
-  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+  HAL_UART_Transmit(&huart2, (uint8_t*)&ch, 1, HAL_MAX_DELAY);
   return 1;
 }
 
@@ -163,44 +164,52 @@ int main(void)
 
   while (1)
   {
-    // Declaration of time and date structures
+    // Define time and date structure variables
     RTC_TimeTypeDef time;
     RTC_DateTypeDef date;
+    // Retrieve current time from the RTC
     HAL_RTC_GetTime(&hrtc, &time, RTC_FORMAT_BIN);
-    HAL_RTC_GetDate(&hrtc, &date, RTC_FORMAT_BIN); 
-    swprintf(time_buffer, sizeof(time_buffer), L"Czas: %02d:%02d:%02d", time.Hours, time.Minutes, time.Seconds);
+    // Retrieve current date from the RTC (date may not be used here but is retrieved for completeness)
+    HAL_RTC_GetDate(&hrtc, &date, RTC_FORMAT_BIN);
+    // Format and assign the current time to the time buffer
+    swprintf(time_buffer, sizeof(time_buffer), L"Time: %02d:%02d:%02d", time.Hours, time.Minutes, time.Seconds);
 
+    // Display the time on the screen at the specified coordinates and font
     hagl_put_text(time_buffer, 15, 20, WHITE, font6x9);
 
+    // Read the battery voltage
     float battery_voltage = read_voltage(&hi2c1);
-    wchar_t voltage_buffer[32]; // Bufor : Voltage
+    wchar_t voltage_buffer[32]; // Buffer for voltage display
 
+    // If the battery voltage is positive, format and display it
     if (battery_voltage > 0)
-      {
-        // Formatting and displaying battery voltage
-        swprintf(voltage_buffer, sizeof(voltage_buffer), L"Napięcie: %.4f V", battery_voltage);
-        // Displaying voltage on the screen with given coordinates and font
-        hagl_put_text(voltage_buffer, 15, 50, WHITE, font6x9); // Change if needed
-      }
-    else
-      {
-        // Additional code for reading battery state of charge (SoC) might go here
-        hagl_put_text(L"Błąd odczytu napięcia!", 15, 50, WHITE, font6x9);
-      }
-
-    float battery_soc = read_soc(&hi2c1);
-    wchar_t soc_buffer[32]; // Bufor SoC
-
-    if (battery_soc >= 0)
     {
-      // Formatowanie i wyświetlanie stanu naładowania baterii
-      swprintf(soc_buffer, sizeof(soc_buffer), L"Naładowanie: %.2f%%", battery_soc);
-      hagl_put_text(soc_buffer, 15, 40, WHITE, font6x9); // Zmienić położenie tekstu w razie potrzeby
+      // Format and display the battery voltage
+      swprintf(voltage_buffer, sizeof(voltage_buffer), L"Voltage: %.4f V", battery_voltage);
+      // Display the voltage on the screen at the specified coordinates and font
+      hagl_put_text(voltage_buffer, 15, 50, WHITE, font6x9); // Adjust text position as needed
     }
     else
     {
-      // Wyświetlanie informacji o błędzie
-      hagl_put_text(L"Błąd odczytu SoC!", 15, 40, WHITE, font6x9);
+      // Display an error message if voltage reading fails
+      hagl_put_text(L"Voltage reading error!", 15, 50, WHITE, font6x9);
+    }
+
+    // Read the battery state of charge (SoC)
+    float battery_soc = read_soc(&hi2c1);
+    wchar_t soc_buffer[32]; // Buffer for SoC display
+
+    // If the SoC reading is valid, format and display it
+    if (battery_soc >= 0)
+    {
+      // Format and display the battery's state of charge
+      swprintf(soc_buffer, sizeof(soc_buffer), L"Charge: %.2f%%", battery_soc);
+      hagl_put_text(soc_buffer, 15, 40, WHITE, font6x9); // Adjust text position as needed
+    }
+    else
+    {
+      // Display an error message if SoC reading fails
+      hagl_put_text(L"SoC reading error!", 15, 40, WHITE, font6x9);
     }
 
     uint8_t ic_version = read_ic_version(&hi2c1);
@@ -218,9 +227,9 @@ int main(void)
       hagl_put_text(L"Błąd odczytu wersji IC!", 15, 80, WHITE, font6x9);
     }
 
-    float resistance = 00.0f;   // Wartość bocznika: 0 Ohm, co oznacza brak bocznika
+    float resistance = 00.0f; // Wartość bocznika: 0 Ohm, co oznacza brak bocznika
     wchar_t current_buffer[64]; // Bufor na prąd
-    wchar_t shunt_buffer[32];   // Bufor na opis bocznika
+    wchar_t shunt_buffer[32]; // Bufor na opis bocznika
 
     // Sprawdzanie czy bocznik jest obecny
     if (resistance > 0.0f)
@@ -391,7 +400,7 @@ void Error_Handler(void)
  * @param  line: assert_param error line source number
  * @retval None
  */
-void assert_failed(uint8_t *file, uint32_t line)
+void assert_failed(uint8_t* file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
